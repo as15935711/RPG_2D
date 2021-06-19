ebfe462699567fd12ce79d7e56c03782003aef23
as15935711 <82298466+as15935711@users.noreply.github.com>
2021-06-19 14:24:07 +0800

    角色

---
+++ RPG_2D/Assets/Scripts/BattleSystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST}
+
+public class BattleSystem : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public GameObject enemyPrefab;
+
+    public Transform playerBattleStation;
+    public Transform enemyBattleStation;
+
+    Unit playerUnit;
+    Unity enemyUnit;
+    public BattleState state;
+    // Start is called before the first frame update
+    void Start()
+    {
+        state = BattleState.START;
+        SetupBattle();
+    }
+    void SetupBattle()
+    {
+       
+       GameObject playerGO = Instantiate(playerPrefab, playerBattleStation);
+        playerGO.GetComponent<U>();
+        Instantiate(enemyPrefab, enemyBattleStation);
+    }
+}
