622c69cfb136a234caa53ea7e26195be46bc7fa3
as15935711 <82298466+as15935711@users.noreply.github.com>
2021-06-19 20:18:15 +0800

    調整

--- RPG_2D/Assets/Scripts/BattleSystem.cs
+++ RPG_2D/Assets/Scripts/BattleSystem.cs
@@ -1,31 +1,145 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST}
+public enum BattleState { START, PLAYERTURN, ENEMYTURN, WON, LOST }
 
 public class BattleSystem : MonoBehaviour
 {
-    public GameObject playerPrefab;
-    public GameObject enemyPrefab;
 
-    public Transform playerBattleStation;
-    public Transform enemyBattleStation;
+	public GameObject playerPrefab;
+	public GameObject enemyPrefab;
 
-    Unit playerUnit;
-    Unity enemyUnit;
-    public BattleState state;
-    // Start is called before the first frame update
-    void Start()
-    {
-        state = BattleState.START;
-        SetupBattle();
-    }
-    void SetupBattle()
-    {
-       
-       GameObject playerGO = Instantiate(playerPrefab, playerBattleStation);
-        playerGO.GetComponent<U>();
-        Instantiate(enemyPrefab, enemyBattleStation);
-    }
+	public Transform playerBattleStation;
+	public Transform enemyBattleStation;
+
+	Unit playerUnit;
+	Unit enemyUnit;
+
+	public Text dialogueText;
+
+	public BattleHUD playerHUD;
+	public BattleHUD enemyHUD;
+
+	public BattleState state;
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		state = BattleState.START;
+		StartCoroutine(SetupBattle());
+	}
+
+	IEnumerator SetupBattle()
+	{
+		GameObject playerGO = Instantiate(playerPrefab, playerBattleStation);
+		playerUnit = playerGO.GetComponent<Unit>();
+
+		GameObject enemyGO = Instantiate(enemyPrefab, enemyBattleStation);
+		enemyUnit = enemyGO.GetComponent<Unit>();
+
+		dialogueText.text = "有一隻 " + enemyUnit.unitName + " 悄悄的接近...";
+
+		playerHUD.SetHUD(playerUnit);
+		enemyHUD.SetHUD(enemyUnit);
+
+		yield return new WaitForSeconds(2f);
+
+		state = BattleState.PLAYERTURN;
+		PlayerTurn();
+	}
+
+	IEnumerator PlayerAttack()
+	{
+		bool isDead = enemyUnit.TakeDamage(playerUnit.damage);
+
+		enemyHUD.SetHP(enemyUnit.currentHP);
+		dialogueText.text = "攻擊成功!";
+
+		yield return new WaitForSeconds(2f);
+
+		if (isDead)
+		{
+			state = BattleState.WON;
+			EndBattle();
+		}
+		else
+		{
+			state = BattleState.ENEMYTURN;
+			StartCoroutine(EnemyTurn());
+		}
+	}
+
+	IEnumerator EnemyTurn()
+	{
+		dialogueText.text = enemyUnit.unitName + "攻擊了!";
+
+		yield return new WaitForSeconds(1f);
+
+		bool isDead = playerUnit.TakeDamage(enemyUnit.damage);
+
+		playerHUD.SetHP(playerUnit.currentHP);
+
+		yield return new WaitForSeconds(1f);
+
+		if (isDead)
+		{
+			state = BattleState.LOST;
+			EndBattle();
+		}
+		else
+		{
+			state = BattleState.PLAYERTURN;
+			PlayerTurn();
+		}
+
+	}
+
+	void EndBattle()
+	{
+		if (state == BattleState.WON)
+		{
+			dialogueText.text = "你贏得了這場戰鬥!";
+		}
+		else if (state == BattleState.LOST)
+		{
+			dialogueText.text = "你屎了 GG";
+		}
+	}
+
+	void PlayerTurn()
+	{
+		dialogueText.text = "選擇行動:";
+	}
+
+	IEnumerator PlayerHeal()
+	{
+		playerUnit.Heal(5);
+
+		playerHUD.SetHP(playerUnit.currentHP);
+		dialogueText.text = "You feel renewed strength!";
+
+		yield return new WaitForSeconds(2f);
+
+		state = BattleState.ENEMYTURN;
+		StartCoroutine(EnemyTurn());
+	}
+
+	public void OnAttackButton()
+	{
+		if (state != BattleState.PLAYERTURN)
+			return;
+
+		StartCoroutine(PlayerAttack());
+	}
+
+	public void OnHealButton()
+	{
+		if (state != BattleState.PLAYERTURN)
+			return;
+
+		StartCoroutine(PlayerHeal());
+	}
+
 }
